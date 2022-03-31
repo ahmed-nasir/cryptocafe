095f89e3683b751e4d6a3e20154388aa674d7520
ahmed-nasir <nasirahmed595@gmail.com>
2022-03-31 22:34:30 +0600

    notFound page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Homepage from './components/Homepage/Homepage'
 import Contact from './components/Contact/Contact'
 import UsAddress from './components/Contact/UsAddress'
 import BdAddress from './components/Contact/BdAddress'
+import Footer from './components/Footer/Footer'
+import NotFound from './components/NotFound/NotFound'
 
 const App = () => {
   return (
@@ -20,7 +22,10 @@ const App = () => {
           <Route path='us-address' element={<UsAddress></UsAddress>}/>
         </Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
+
+      <Footer/>
     </div>
   )
 }
