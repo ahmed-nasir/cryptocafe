2854dcfe5e11d94c7eaea79a8461f919de5a2737
ahmed-nasir <nasirahmed595@gmail.com>
2022-03-31 18:12:40 +0600

    custom link worked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import About from './components/About/About'
 import Coins from './components/Coins/Coins'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -11,7 +13,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Homepage></Homepage>}></Route>
         <Route path='/coins' element={<Coins></Coins>}></Route>
-        
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
       </Routes>
     </div>
   )
